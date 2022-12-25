3be7a2393ca031813847c3ec80330ebea043cf72
slava <volkron53@gmail.com>
2022-12-25 20:25:53 +0300

    Vnutrennie class

--- app/src/main/java/com/example/myapplicationmsk/Cat.java
+++ app/src/main/java/com/example/myapplicationmsk/Cat.java
@@ -1,4 +1,5 @@
 package com.example.myapplicationmsk;
+
 import android.util.Log;
 
 //Inner class
@@ -6,13 +7,15 @@ import android.util.Log;
 //Local class
 //Anonymous class
 
-public class Cat extends Animal{
-    int age;
+import java.security.PrivateKey;
+
+public class Cat extends Animal {
+    private int age;
     String name;
-    private String color;
-    private String breed;
-    final static int numberOfLegs= 4;
-    static int count = 0;
+    final static int numOfLeg = 4;
+    private int color;
+    private int breed;
+    static int cout = 0;
 
     String helloText;
     CatMood catMood;
@@ -21,66 +24,62 @@ public class Cat extends Animal{
         int levelOfMood;
 
         CatMood() {
-            if(Cat.this.age < 2){
+            if(Cat.this.age < 2) {
                 levelOfMood = 100;
-            }else if(Cat.this.age >= 2 && Cat.this.age < 7) {
+            } else if (Cat.this.age >= 2 && Cat.this.age < 7){
                 levelOfMood = 50;
-            }else if (Cat.this.age >= 7) {
+            } else if (Cat.this.age >= 7) {
                 levelOfMood = 20;
             }
+
         }
     }
-    public Cat()  {
-        this.name = "John Cena";
+
+
+    public Cat() {
         this.age = 1;
+        this.name = "John Cena";
 
         catMood = new CatMood();
-
-        switch (catMood.levelOfMood) {
+        switch (catMood.levelOfMood){
             case 100:
-                helloText = "Meow! Im young and happy cat!:) My name is " + name + ", and i`m " + age + " years old.";
+                helloText = "Meow! I'm  happy cat:) My name is " + name + ", and I'm " + age + "years old.";
                 break;
             case 50:
-                helloText = "Meow! Im happy cat! My name is " + name + ", and i`m " + age + " years old.";
+                helloText = "Meow! I'm cat. My name is " + name + ", and I'm " + age + "years old.";
                 break;
             case 20:
-                helloText = "Meow! Im old and sick cat:( My name is " + name + ", and i`m " + age + " years old.";
+                helloText = "Meow! I'm  old and sick happy cat:( My name is " + name + ", and I'm " + age + "years old.";
                 break;
 
         }
-
     }
-    public Cat(String color, String breed) {
-        this.color = color;
-        this.breed = breed;
 
-        catMood = new CatMood();
-
-
-    }
     public Cat(int age, String name) {
         this.age = age;
         this.name = name;
+        this.color = color;
+        this.breed = breed;
         catMood = new CatMood();
 
         switch (catMood.levelOfMood) {
             case 100:
-                helloText = "Meow! Im young and happy cat!:) My name is " + name + ", and i`m " + age + " years old.";
+                helloText = "Meow! I'm  happy cat:) My name is " + name + ", and I'm " + age + "years old.";
                 break;
             case 50:
-                helloText = "Meow! Im happy cat! My name is " + name + ", and i`m " + age + " years old.";
+                helloText = "Meow! I'm cat. My name is " + name + ", and I'm " + age + "years old.";
                 break;
             case 20:
-                helloText = "Meow! Im old and sick cat:( My name is " + name + ", and i`m " + age + " years old.";
+                helloText = "Meow! I'm  old and sick happy cat:( My name is " + name + ", and I'm " + age + "years old.";
                 break;
 
         }
     }
 
-
-    public void talk(){
-        Log.i("talk()",  helloText);
+    public void talk() {
+        Log.i(" talk()", helloText);
     }
+
     public void talk(int age) {
         Log.i(" talk()", "Meow! I'm" + name + ", and I'm " + age + "years old.");
     }
--- app/src/main/java/com/example/myapplicationmsk/Lion.java
+++ app/src/main/java/com/example/myapplicationmsk/Lion.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 public class Lion extends  Cat{
 
+
     public static String whatCatsLike(){
         return "I'm Lion and I like playing, jumping and sometimes scratching";
     }
