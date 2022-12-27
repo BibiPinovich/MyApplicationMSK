de48b3e26c30c851e9a5b8a8fb0130ae8e80d140
slava <volkron53@gmail.com>
2022-12-27 12:44:39 +0300

    Vnutrennie class

--- app/src/main/java/com/example/myapplicationmsk/MainActivity.java
+++ app/src/main/java/com/example/myapplicationmsk/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.myapplicationmsk;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -9,8 +12,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Cat cat1 = new Cat();
-        cat1.catchMouse(1);
-        cat1.catchMouse(3);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Cat singingCat  = new Cat(){
+            @Override
+            public void talk(){
+                Log.i("talk()",  "I'm singing! La-la-la!");
+            }
+        };
+        singingCat.talk();
+
+        Cat cat = new Cat();
+        cat.talk();
     }
 }
